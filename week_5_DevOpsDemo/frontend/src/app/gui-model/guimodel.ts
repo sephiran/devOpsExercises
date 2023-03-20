f86a1ce74aaef27871227427844c386807079fd0
Adrian Peter <adrian.Maximilian.peter@gmail.com>
2023-03-20 13:22:30 +0100

    week 1 - 5

---
+++ week_5_DevOpsDemo/frontend/src/app/gui-model/guimodel.ts
@@ -0,0 +1,177 @@
+/* tslint:disable:max-line-length */
+export class GuiModel {
+
+    private _guiModel = {
+        "application": {
+            "title": "DevOpsDemo FS2023",
+            "formList": [
+                {
+                    "id": "OwnUserForm",
+                    "title": "NotImplemented",
+                    "formFieldList": [
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                    ]
+                },
+                {
+                    "id": "NotImplementedForm",
+                    "title": "Not Implemented",
+                    "url": "/dummyform",
+                    "headerVisible": false,
+                    "footerVisible": false,
+                    "borderStyle": "None",
+                    "formFieldList": [
+                        {
+                            "id": "NotImplemented",
+                            "type": "label",
+                            "name": "NotImplementedMessage",
+                            "width": 2
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                    ]
+                },
+                {
+                    "id": "NotImplementedFormModal",
+                    "title": "NotImplemented",
+                    "url": "/dummyform",
+                    "formFieldList": [
+                        {
+                            "id": "NotImplemented",
+                            "type": "label",
+                            "name": "NotImplementedMessage",
+                            "width": 2
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                    ]
+                },
+                {
+                    "id": "DeleteAllForm",
+                    "title": {default: "Do you want to delete all entries?"},
+                    "url": "/deleteAll",
+                    "formFieldList": [
+                        {
+                            "type": "okButton",
+                            "name": "Delete all"
+                        }
+                    ]
+                },
+                {
+                    "id": "ToDoForm",
+                    "title": { default: "ToDo" },
+                    "url": "/todo",
+                    "formFieldList": [
+                        {
+                            "id":   "title",
+                            "type": "text",
+                            "name": { default: "Titel" },
+                            "required": true,
+                            "width": 2
+                        },
+                        {
+                            "id":   "responsiblePerson",
+                            "type": "text",
+                            "name": { default: "Zuständige Person" },
+                            "required": true,
+                            "width": 2
+                        },
+                        {
+                            "id": "description",
+                            "type": "text",
+                            "name": { default: "Text" },
+                            "newRow": true,
+                            "maxLength": 4000,
+                            "height": 4,
+                            "width": 2
+                        },
+                        {
+                            "type": "deleteButton",
+                            "name": "Delete"
+                        },
+                        {
+                            "type": "cancelButton",
+                            "name": "Cancel"
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                    ]
+                },
+            ],
+            "pageList": [
+                {
+                    "id": "mainmenu",
+                    "name": "MainMenu",
+                    "elementList": [
+                        {
+                            "type": "pageLabel",
+                            "value": "<h4>Willkommen bei DevOps</h4>",
+                            "newRow": true
+                        },
+                        {
+                            "type": "button",
+                            "name": { default: "ToDo-List" },
+                            "icon": "fa-file-alt",
+                            "color": "wet-asphalt",
+                            "page": "toDoPage",
+                            "width": 2,
+                            "newRow": true,
+                        },
+                    ]
+                },
+                {
+                    "id": "toDoPage",
+                    "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": { default: "Neues ToDo"},
+                            "icon": "fa-user",
+                            "color": "green",
+                            "width": 2,
+                            "form" : {
+                                "form" : "ToDoForm"
+                            }
+                        },
+                        {
+                            "type": "list",
+                            "name": "ToDo",
+                            "icon": "fa-user",
+                            "color": "wet-asphalt",
+                            "search": true,
+                            "url": "/todo",
+                            "form": {
+                                "form": "ToDoForm"
+                            }
+                        },
+                        {
+                            "type": "newButton",
+                            "name": { default: "Delete all"},
+                            "icon": "fa-trash",
+                            "color": "red",
+                            "width": 2,
+                            "form" : {
+                                "form" : "DeleteAllForm"
+                            }
+                        },
+                    ]
+                },
+            ]
+        }
+    };
+
+
+    get guiModel() {
+        return this._guiModel;
+    }
+}
